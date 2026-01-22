3edf1d616ce923d7dbd56a29640ab8569cf459d4
Mavric Lau Wai Xian <24045833@myrp.edu.sg>
2026-01-22 10:11:44 +0800

    Use environment variable for server port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
